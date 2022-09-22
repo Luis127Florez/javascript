f1ff68417160609fb42f1fd2402809b9f25eaffe
Luis127Florez <florezgilluisantony@gmail.com>
2022-09-22 17:56:38 +0200

    subida de archivos javascript

---
+++ ejemplo.js
@@ -0,0 +1,14 @@
+function soloString (array){
+    
+    for (let i = 0; i < array.length; i++) {
+        var C = array[i]
+
+        if ( isNaN(C) ) {
+            console.log( array[i])
+        }        
+    }   
+}
+
+const array = [1, "Henry", 2,"Tony"]
+
+soloString(array)
---
+++ wtresscool.js
@@ -0,0 +1,22 @@
+const array =[2,4,6,4,4,4,4,4,4,7,7,7].sort();
+
+var j = 0
+
+var Nrepe = []
+
+for (let i = 0; i < array.length; i++) {
+    var veces = 0
+    
+    for (let c = 0; c < array.length; c++) {
+        if (array [i] == array [c]) {
+            j++;
+
+            if (j > veces) {
+                veces = j
+                console.log(veces)
+            }
+        }     
+    }
+    j-=j
+}
+
