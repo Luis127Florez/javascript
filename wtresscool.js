d52cddd819fa41eb77f6b2a95e92e3306007e12f
Luis127Florez <florezgilluisantony@gmail.com>
2022-09-23 11:49:16 -0500

    cambio en w3ccoll

--- wtresscool.js
+++ wtresscool.js
@@ -1,22 +1,7 @@
-const array =[2,4,6,4,4,4,4,4,4,7,7,7].sort();
+const array =[2,4,6,4,3,4,4,3,4,4,4,7,7,7].sort();
 
-var j = 0
-
-var Nrepe = [];
-var po = 0;
+let con = 0 ;
 for (let i = 0; i < array.length; i++) {
-    var veces = 0
+    console.log(array[i]);
     
-    for (let c = 0; c < array.length; c++) {
-        if (array [i] == array [c]) {
-            j++;
-
-            if (j > veces) {
-                veces = j
-                console.log(veces)
-            }
-        }     
-    }
-    j-=j
 }
-
