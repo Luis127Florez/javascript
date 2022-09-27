d2a375eaa29e7b2acd2ea5d1fdff245dec445fc2
Luis127Florez <florezgilluisantony@gmail.com>
2022-09-27 14:26:53 -0500

    array achivo

---
+++ arrayEjercicio.js
@@ -0,0 +1,10 @@
+let array =[1,4,65,8,8,9,67,6,] ;
+array.splice(2, 0, 5);
+
+/* array.forEach((element)=>{
+    console.log(element)
+});
+ */
+
+let Narray = array.map(i => i + 3 );
+console.log(Narray);
