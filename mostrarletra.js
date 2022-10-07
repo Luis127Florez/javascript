8227183af21ffac719173c7fd9ecac19b9da02f7
Luis127Florez <florezgilluisantony@gmail.com>
2022-10-07 18:01:08 -0500

    mostrar letra

---
+++ mostrarletra.js
@@ -0,0 +1,13 @@
+let nombre = "luis";
+let numero = 3;
+
+let letra = mostrarLetra(nombre, numero);
+
+console.log(letra);
+
+function mostrarLetra (nombre , numero) {
+    
+    return nombre.charAt(numero);
+
+
+}
